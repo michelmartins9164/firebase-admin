eb69f080fceea07b7d4eae2c4484b01ede98094c
Michel Martins <michelmartins9164@gmail.com>
2025-05-20 20:59:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(express.json());
+
+app.post("/send-notification", async (req, res) => {
+  const { token, title, body } = req.body;
+
+  const message = {
+    notification: { title, body },
+    token,
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    res.status(200).send(`Mensagem enviada com sucesso: ${response}`);
+  } catch (error) {
+    res.status(500).send(`Erro ao enviar: ${error}`);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("🔥 Servidor rodando em http://localhost:3000");
+});
